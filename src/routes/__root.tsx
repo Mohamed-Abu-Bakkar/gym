0ca1adae36e46eaf6c37ef456b72624cc11fe07b
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-01-18 02:18:22 +0530

    clean up

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,8 +6,6 @@ import {
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
-
 import ConvexProvider from '../integrations/convex/provider'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
--- src/routes/app/_components/bottom-bar-admin.tsx
+++ src/routes/app/_components/bottom-bar-admin.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/app/components/bottom-bar-admin')({
+export const Route = createFileRoute('/app/_components/bottom-bar-admin')({
   component: RouteComponent,
 })
 
